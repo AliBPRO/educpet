9a9524cbf99a371a175c0a13fd7c4c1ef00235f2
AliBPRO <59185023+AliBPRO@users.noreply.github.com>
2022-05-30 17:52:42 +0200

    hehe

---
+++ educpet_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EducpetAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'educpet_app'
---
+++ educpet_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.core.mail import EmailMessage
+from django.http.response import JsonResponse
+
+
+# Create your views here.
+def index(request):
+	context = locals()
+	template = 'index.html'
+	return render(request, template, context)
+
+def send_information_email(request):
+	sujet = "Demande site : " + request.POST.get('name')
+	email_client = request.POST.get('email')
+	message = request.POST.get('message') + "\n\nEmail du client : " + email_client
+	
+	email = EmailMessage(sujet, message, to=['educpet@gmail.com'])
+	email.send()
+
+	return JsonResponse({})
