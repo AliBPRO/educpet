126b10b1475770a318a118e5beba6cbe2eee2fe1
AliBPRO <59185023+AliBPRO@users.noreply.github.com>
2022-05-30 17:14:36 +0200

    email

--- educpet/educpet/settings.py
+++ educpet/educpet/settings.py
@@ -28,6 +28,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'educpet@gmail.com'
+EMAIL_HOST_PASSWORD = 'La2605Ma.'
 
 # Application definition
 
--- educpet/educpet/urls.py
+++ educpet/educpet/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('send_information_email/', views.send_information_email),
 ]
 
 if settings.DEBUG:
